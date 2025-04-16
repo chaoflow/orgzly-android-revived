2f5820241e6f0751f0a5a2d30c12c48b175615be
Florian Friesdorf <florian@ternaris.com>
2025-04-16 15:19:32 +0200

    [nix] Use kotlin19

--- flake.nix
+++ flake.nix
@@ -25,6 +25,40 @@
           platformVersions = [ "34" ];
         };
 
+        # We're on kotlin 1.9 for now:
+        # https://github.com/orgzly-revived/orgzly-android-revived/issues/454
+        jre = pkgs.jdk21_headless;
+        kotlin19 = pkgs.stdenv.mkDerivation (finalAttrs: {
+          pname = "kotlin";
+          version = "1.9.24";
+
+          src = pkgs.fetchurl {
+            url = "https://github.com/JetBrains/kotlin/releases/download/v${finalAttrs.version}/kotlin-compiler-${finalAttrs.version}.zip";
+            sha256 = "sha256-63to4BAp+me8jQYO5UwSAY8sYN3EOM8h2xRRcimqaTs=";
+          };
+
+          propagatedBuildInputs = [ jre ];
+          nativeBuildInputs = [
+            pkgs.makeWrapper
+            pkgs.unzip
+          ];
+
+          installPhase = ''
+            mkdir -p $out
+            rm "bin/"*.bat
+            mv * $out
+
+            for p in $(ls $out/bin/) ; do
+              wrapProgram $out/bin/$p --prefix PATH ":" ${jre}/bin ;
+            done
+
+            if [ -f $out/LICENSE ]; then
+              install -D $out/LICENSE $out/share/kotlin/LICENSE
+              rm $out/LICENSE
+            fi
+          '';
+        });
+
       in
       {
         inherit androidComposition;
@@ -36,8 +70,8 @@
             buildInputs = with pkgs; [
               google-java-format
               gradle_8
-              jdk21_headless # match with kotlin's jdk
-              kotlin
+              jre
+              kotlin19
               kotlin-language-server
               ktlint
             ];
