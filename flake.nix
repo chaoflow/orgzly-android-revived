5e031e941f89ac38fa796fee7eadb5bd1c6fc5a9
Florian Friesdorf <florian@ternaris.com>
2025-04-16 15:09:16 +0200

    [nix] nixfmt flake.nix

--- flake.nix
+++ flake.nix
@@ -4,40 +4,45 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
-  flake-utils.lib.eachDefaultSystem (system: let
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        android_sdk.accept_license = true;
-        allowUnfree = true;
-      };
-    };
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            android_sdk.accept_license = true;
+            allowUnfree = true;
+          };
+        };
 
-    androidComposition = pkgs.androidenv.composeAndroidPackages {
-      platformVersions = [ "34" ];
-    };
+        androidComposition = pkgs.androidenv.composeAndroidPackages {
+          platformVersions = [ "34" ];
+        };
 
-  in {
-    inherit androidComposition;
+      in
+      {
+        inherit androidComposition;
 
-    devShells = {
-      default = pkgs.mkShell rec {
-        ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+        devShells = {
+          default = pkgs.mkShell rec {
+            ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
 
-        buildInputs = with pkgs; [
-          google-java-format
-          gradle_8
-          jdk21_headless  # match with kotlin's jdk
-          kotlin
-          kotlin-language-server
-          ktlint
-        ];
-      };
-    };
-  });
+            buildInputs = with pkgs; [
+              google-java-format
+              gradle_8
+              jdk21_headless # match with kotlin's jdk
+              kotlin
+              kotlin-language-server
+              ktlint
+            ];
+          };
+        };
+      }
+    );
 }
